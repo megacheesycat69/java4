7914a573773a12a27cfdbe9601058d0955efed9e
cheesycat69 <91827997+megacheesycat69@users.noreply.github.com>
2026-05-22 15:46:33 +0300

    Add files via upload

---
+++ Task2_1_2.java
@@ -0,0 +1,6 @@
+public class Task2_1_2 {
+    public static void main(String[] args) {
+        // Печать имени "Артём" с помощью Unicode-последовательностей
+        System.out.println("\u0410\u0440\u0442\u0451\u043C");
+    }
+}
