42fecce8dbbd34b9b60c8fa55b667ab85b1d930a
cheesycat69 <91827997+megacheesycat69@users.noreply.github.com>
2026-05-22 15:47:04 +0300

    Add files via upload

---
+++ Task2_1_4.java
@@ -0,0 +1,11 @@
+public class Task2_1_4 {
+    public static void main(String[] args) {
+        String str = "Сумма: ";
+        int a = 5;
+        double b = 3.2;
+        Object obj = new Object();
+        
+        String result = str + a + ", " + b + ", объект: " + obj;
+        System.out.println(result);
+    }
+}
